3cb80140a6d50ec770602bf799cb07f1624b6257
WolfCub15 <volkovanastyavolkova@gmail.com>
2021-03-15 11:47:07 +0300

    spaces

--- modules/complex-number/test/test_volkova_nastya_complex_number.cpp
+++ modules/complex-number/test/test_volkova_nastya_complex_number.cpp
@@ -20,38 +20,39 @@ TEST(Volkova_Nastya_ComplexNumberTest, AssignmentOperator) {
     ComplexNumber numb3;
     numb3.setRe(numb_re);
     numb3.setIm(numb_im);
-    
+
     EXPECT_EQ(numb1.getRe(), numb2.getRe());
     EXPECT_EQ(numb1.getIm(), numb2.getIm());
     EXPECT_EQ(numb1.getRe(), numb3.getRe());
     EXPECT_EQ(numb1.getIm(), numb3.getIm());
 }
 
-typedef testing::TestWithParam<std::tuple<double, double,double,double,double,double> > Volkova_Parametrized;
+typedef testing::TestWithParam<std::tuple< double, double, double, double, double, double>>
+        Volkova_Parametrized;
 
 TEST_P(Volkova_Parametrized, Sum) {
-    ComplexNumber numb1(std::get<0>(GetParam()),std::get<1>(GetParam()));
-    ComplexNumber numb2(std::get<2>(GetParam()),std::get<3>(GetParam()));
-    ComplexNumber numb3(std::get<4>(GetParam()),std::get<5>(GetParam()));
+    ComplexNumber numb1(std::get<0>(GetParam()), std::get<1>(GetParam()));
+    ComplexNumber numb2(std::get<2>(GetParam()), std::get<3>(GetParam()));
+    ComplexNumber numb3(std::get<4>(GetParam()), std::get<5>(GetParam()));
 
-    ComplexNumber res1 = numb1+numb2;
-    ComplexNumber res2 = numb1+numb3;
-    ComplexNumber res3 = res2-res1;
+    ComplexNumber res1 = numb1 + numb2;
+    ComplexNumber res2 = numb1 + numb3;
+    ComplexNumber res3 = res2 - res1;
     ComplexNumber res4 = res3 + numb2;
 
     EXPECT_EQ(res4, numb3);
 }
 
 TEST_P(Volkova_Parametrized, MultiplicationAndSum) {
-    ComplexNumber numb1(std::get<0>(GetParam()),std::get<1>(GetParam()));
-    ComplexNumber numb2(std::get<2>(GetParam()),std::get<3>(GetParam()));
-    ComplexNumber numb3(std::get<4>(GetParam()),std::get<5>(GetParam()));
-    
-    ComplexNumber res1 = numb1*numb2;
-    ComplexNumber res2 = numb1*numb3;
+    ComplexNumber numb1(std::get<0>(GetParam()), std::get<1>(GetParam()));
+    ComplexNumber numb2(std::get<2>(GetParam()), std::get<3>(GetParam()));
+    ComplexNumber numb3(std::get<4>(GetParam()), std::get<5>(GetParam()));
+
+    ComplexNumber res1 = numb1 * numb2;
+    ComplexNumber res2 = numb1 * numb3;
     ComplexNumber res3 = res1 + res2;
     ComplexNumber res4 = numb2 + numb3;
-    ComplexNumber res5 = numb1*res4;
+    ComplexNumber res5 = numb1 * res4;
 
     EXPECT_EQ(res3, res5);
 }
